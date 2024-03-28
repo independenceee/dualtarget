82a5b4b688d3c7112774bf753eb2f2fedaa4e6b6
Nguyễn Khánh <108068667+independenceee@users.noreply.github.com>
2024-03-28 11:19:58 +0700

    develop

---
+++ src/utils/history-price.ts
@@ -0,0 +1,28 @@
+import { BlockFrostAPI } from "@blockfrost/blockfrost-js";
+import { BlockfrostAdapter, NetworkId } from "@minswap/sdk";
+
+const historyPrice = async function () {
+    const MIN_ADA_POOL_ID = "6aa2153e1ae896a95539c9d62f76cedcdabdcdf144e564b8955f609d660cf6a2";
+
+    const api = new BlockfrostAdapter({
+        blockFrost: new BlockFrostAPI({
+            projectId: process.env.BLOCKFROST_PROJECT_API_KEY_MAINNET!,
+            network: "mainnet",
+        }),
+    });
+    const history = await api.getPoolHistory({ id: MIN_ADA_POOL_ID });
+    for (const historyPoint of history) {
+        const pool = await api.getPoolInTx({ txHash: historyPoint.txHash });
+        if (!pool) {
+            return
+        }
+        const [price0, price1] = await api.getPoolPrice({
+            pool,
+            decimalsA: 6,
+            decimalsB: 6,
+        });
+        console.log(`${historyPoint.time}: ${price0} ADA/MIN, ${price1} MIN/ADA`);
+    }
+};
+
+export default historyPrice;
