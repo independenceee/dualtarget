1acff5fe90e5c2b600ae884935e42650a6da4831
Nguyễn Khánh <108068667+independenceee@users.noreply.github.com>
2024-04-06 01:46:08 +0700

    develop

--- frontend/src/utils/read-validator.ts
+++
@@ -1,9 +0,0 @@
-import { SpendingValidator } from "lucid-cardano";
-import { dualTargetCompilecode } from "~/libs";
-
-const readValidator = function (): SpendingValidator {
-    if (!dualTargetCompilecode) throw new Error("Dualtarget compile code is not found");
-    return { type: "PlutusV2", script: dualTargetCompilecode };
-};
-
-export default readValidator;
